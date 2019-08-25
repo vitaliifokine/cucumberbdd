39c769e52dac5936904dd22db01f257d63ab0d05
Vitalii Fokin <vitaliiifokine@gmail.com>
2019-08-25 11:39:55 -0400

    Selenium Addition V

--- src/test/java/bdd/cucumber/RunCucumberTest.java
+++ src/test/java/bdd/cucumber/RunCucumberTest.java
@@ -5,6 +5,6 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = "pretty", features = "src\\test\\resources\\bdd\\cucumber\\WordpressMainPage.feature")
+@CucumberOptions(plugin = "pretty", features = "src\\test\\resources\\bdd\\cucumber")
 public class RunCucumberTest {
 }
