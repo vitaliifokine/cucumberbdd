9662883bc41e1c160571f76888bf2316a7f174a0
vitaliifokine@gmail.com <vf281992>
2021-03-04 16:13:36 -0500

    Driver update

--- src/test/java/bdd/cucumber/StartPageStepdefs.java
+++ src/test/java/bdd/cucumber/StartPageStepdefs.java
@@ -3,6 +3,7 @@ package bdd.cucumber;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,7 +12,7 @@ public class StartPageStepdefs {
     WebDriver driver;
 
     public void launchTheBrowser(){
-        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
         driver= new ChromeDriver();
         driver.manage().window().maximize();
     }
